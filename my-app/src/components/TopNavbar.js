3c41abb7967ba0572e96060440022a5d6c03f654
Vang Xiong <vangx709@gmail.com>
2022-12-15 16:10:39 -0900

    added 2 components TopNavbar and DiscordContact

---
+++ my-app/src/components/TopNavbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TopNavbar.css";
+
+export default function TopNavbar() {
+  return (
+    <nav className="TopNavbar">
+      <h1>The Art</h1>
+      <h1>The Artist</h1>
+      <h1>Galleries</h1>
+    </nav>
+  );
+}
