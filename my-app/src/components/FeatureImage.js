15583f6fb143898048730b1d38434db82c2b2aef
Vang Xiong <vangx709@gmail.com>
2022-12-21 11:58:25 -0900

    Working onClick image

--- my-app/src/components/FeatureImage.js
+++ my-app/src/components/FeatureImage.js
@@ -1,32 +1,55 @@
 import React from "react";
 import "./FeatureImage.css";
-import displayImage from "../images/Originalv2.png";
-import displayImage1 from "../images/v2UVS1.png";
-import displayImage2 from "../images/v2UVS2.png";
-import displayImage3 from "../images/v2UVS3.png";
+import { data } from "../images/images.js";
 
-export default function FeatureImage() {
-  return (
-    <section className="displayed-art-container">
+export default class FeatureImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: "armorCat",
+    };
+  }
+
+  currentImageDisplay = (event) => {
+    //clear
+    let x = document.getElementById("displayed-art");
+    x.innerHTML = "";
+
+    //set new
+    let newIMG = document.createElement("img");
+    newIMG.src = event.target.src;
+    newIMG.className = "displayed-art";
+
+    x.appendChild(newIMG);
+  };
+  render() {
+    const slides = data[this.state.theme].map((i) => (
       <img
-        src={displayImage}
-        alt="displayed-art"
-        className="displayed-art"
+        src={i}
+        alt="slides-displayed-art"
+        className="slides"
+        onClick={this.currentImageDisplay}
+        key={i}
       ></img>
-      <div className="displayed-art-title">
-        <h2>Midjourney</h2>
-        <h6 className="displayed-art-subtitle">empowering people</h6>
-      </div>
+    ));
 
-      <div className="slides-container">
-        <img src={displayImage1} alt="displayed-art" className="slides"></img>
-        <img
-          src={displayImage2}
-          alt="displayed-art"
-          className="slides-center"
-        ></img>
-        <img src={displayImage3} alt="displayed-art" className="slides"></img>
-      </div>
-    </section>
-  );
+    return (
+      <section className="displayed-art-container">
+        <div id="displayed-art">
+          <img
+            src={data.armorCat[0]}
+            alt="displayed-art"
+            className="displayed-art"
+          ></img>
+        </div>
+
+        <div className="displayed-art-title">
+          <h2>Midjourney</h2>
+          <h6 className="displayed-art-subtitle">empowering people</h6>
+        </div>
+
+        <div className="slides-container">{slides}</div>
+      </section>
+    );
+  }
 }
